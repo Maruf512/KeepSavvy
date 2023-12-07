8578e3a91b47d9b91d023fc8c034fb113d4831b1
Maruf512 <marufsarkar512@gmail.com>
2023-12-07 13:03:08 +0600

    not changing view

--- KeepSavvy/MVVM/ViewModel/MainViewModel.cs
+++ KeepSavvy/MVVM/ViewModel/MainViewModel.cs
@@ -1,12 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using KeepSavvy.Core;
 
 namespace KeepSavvy.MVVM.ViewModel
 {
-    internal class MainViewModel
+    class MainViewModel : ObservableObject
     {
+        // to bind with radio button
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand AddViewCommand { get; set; }
+        public RelayCommand DispatchViewCommand { get; set; }
+        public RelayCommand ModifyViewCommand { get; set; }
+        public RelayCommand RemoveViewCommand { get; set; }
+        public RelayCommand ReturnViewCommand { get; set; }
+
+        // view page models
+        public HomeViewModel HomeVM { get; set; }
+        public AddViewModel AddVM { get; set; }
+        public DispatchViewModel DispatchVM { get; set; }
+        public ModifyViewModel ModifyVM { get; set; }
+        public RemoveViewModel RemoveVM { get; set; }
+        public ReturnViewModel ReturnVM { get; set; }
+
+        // function to change View
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // main function
+        public MainViewModel()
+        {
+            // instance of view file
+            HomeVM = new HomeViewModel();
+            AddVM = new AddViewModel();
+            DispatchVM = new DispatchViewModel();
+            ModifyVM = new ModifyViewModel();
+            RemoveVM = new RemoveViewModel();
+            ReturnVM = new ReturnViewModel();
+
+            // launch main home view
+            CurrentView = HomeVM;
+
+            // change betwen views
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            AddViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = AddVM;
+            });
+
+            DispatchViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DispatchVM;
+            });
+
+            ModifyViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ModifyVM;
+            });
+
+            RemoveViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = RemoveVM;
+            });
+
+            ReturnViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ReturnVM;
+            });
+
+
+        }
+
     }
 }
--- KeepSavvy/MainWindow.xaml.cs
+++ KeepSavvy/MainWindow.xaml.cs
@@ -1,28 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace KeepSavvy
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void ExitApp(object sender, MouseButtonEventArgs e)
+        {
+                Application.Current.Shutdown();
         }
     }
 }
