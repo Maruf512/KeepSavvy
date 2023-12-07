0f096dda0028c1d197b74f5f8b7e6720e5295c58
Maruf512 <marufsarkar512@gmail.com>
2023-12-07 15:24:55 +0600

    adding datagrid

---
+++ KeepSavvy/MVVM/Model/ProductModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeepSavvy.MVVM.Model
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public double PerCarton { get; set; }
+        public double DP { get; set; }
+        public double TP { get; set; }
+        public int DispatchAmount { get; set; }
+        public int ReturnAmount { get; set; }
+        public int SaleAmount { get; set; }
+    }
+}
